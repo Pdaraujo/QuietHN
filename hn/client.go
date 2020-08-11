02a706d420777885cf37e36249e2e6cd5ff0ca1d
diogoaraujo <diogo.araujo@twisper.com>
2020-08-11 16:08:16 +0100

    added initial setup

--- hn/client.go
+++ hn/client.go
@@ -11,6 +11,27 @@ const (
 	topStories = "/topstories.json"
 )
 
+// Item represents a single item returned by the HN API. This can have a type
+// of "story", "comment", or "job" (and probably more values), and one of the
+// URL or Text fields will be set, but not both.
+//
+// For the purpose of this exercise, we only care about items where the
+// type is "story", and the URL is set.
+type Item struct {
+	By          string `json:"by"`
+	Descendants int    `json:"descendants"`
+	ID          int    `json:"id"`
+	Kids        []int  `json:"kids"`
+	Score       int    `json:"score"`
+	Time        int    `json:"time"`
+	Title       string `json:"title"`
+	Type        string `json:"type"`
+
+	// Only one of these should exist
+	Text string `json:"text"`
+	URL  string `json:"url"`
+}
+
 type Client struct {
 	apiBase string
 	topStories string
@@ -31,7 +52,7 @@ func (c *Client) setDefaults() {
 	c.topItemsURL = fmt.Sprintf("%s%s", c.apiBase, c.topStories)
 }
 
-func (c *Client) getItemURL(id int) string {
+func (c *Client) createItemURL(id int) string {
 	c.setDefaults()
 	return fmt.Sprintf("%s/item/%d.json", c.apiBase, id)
 }
@@ -58,4 +79,21 @@ func (c *Client) TopItems() ([]int, error) {
 	}
 
 	return ids, nil
+}
+
+// GetItem will return the Item defined by the provided ID.
+func (c *Client) GetItem(id int) (Item, error) {
+	var item Item
+	resp, err := http.Get(c.createItemURL(id))
+	if err != nil {
+		return item, err
+	}
+
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&item)
+	if err != nil {
+		return item, err
+	}
+	return item, nil
 }
